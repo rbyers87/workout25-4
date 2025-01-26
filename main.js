04b1789ba75fcce814ee3e5d879bcfde40160b6e
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-01-26 02:32:31 -0600

    Update main.js

--- main.js
+++ main.js
@@ -18,16 +18,3 @@ if (isStandalone) {
   // Listen for system theme changes
   darkModeMediaQuery.addEventListener('change', updateTheme);
 }
-
-// Service Worker Registration (if you have one)
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(function(registration) {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch(function(err) {
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  });
-}
